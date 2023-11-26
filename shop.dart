f7d1a48b933e139f0f457addbb242552332da5b8
FatmehAT <152075073+FatmehAT@users.noreply.github.com>
2023-11-26 15:31:55 +0200

    Create shop.dart

---
+++ shop.dart
@@ -0,0 +1,35 @@
+import 'package:coffee/coffee.dart';
+import 'package:flutter/material.dart';
+
+class Shop extends StatefulWidget {
+  const Shop({
+    super.key,
+  });
+
+  @override
+  State<Shop> createState() => _ShopState();
+}
+
+class _ShopState extends State<Shop> {
+  Coffee coffee = Coffee(name: '', description: '', price: 0.0);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(coffee.name),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 16),
+            Text('Description: ${coffee.description}'),
+            SizedBox(height: 8),
+            Text('Price: \$${coffee.price.toStringAsFixed(2)}'),
+          ],
+        ),
+      ),
+    );
+  }
+}
