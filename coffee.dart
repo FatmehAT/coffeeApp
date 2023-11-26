9dbe3f23a43c835144291be0a6f28d8105744834
FatmehAT <152075073+FatmehAT@users.noreply.github.com>
2023-11-26 15:33:42 +0200

    Create coffee.dart

---
+++ coffee.dart
@@ -0,0 +1,13 @@
+class Coffee {
+  String name;
+  String description;
+  double price;
+
+  Coffee({
+    required this.name,
+    required this.description,
+    required this.price,
+  });
+}
+
+List<Coffee> coffeeItems = [];
