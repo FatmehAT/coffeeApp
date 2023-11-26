25860924e97f69c3db8274032489c0337ebeedec
FatmehAT <152075073+FatmehAT@users.noreply.github.com>
2023-11-26 15:31:32 +0200

    Create home.dart

---
+++ home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'coffee_shop.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CoffeeShop(),
+    );
+  }
+}
